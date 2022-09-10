fa5f3175f11812cdaef02e81e6fc448b984a7231
rob tillaart <rob.tillaart@gmail.com>
2022-09-10 17:37:38 +0200

    test fletcher16 for 0.1.6

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -83,6 +83,50 @@ unittest(test_fletcher64)
 }
 
 
+unittest(test_FF_1)
+{
+  uint8_t x[1] = 0xFF;
+  uint16_t y[1] = 0xFFFF;
+  uint32_t z[1] = 0xFFFFFFFF;
+  
+  assertEqual(0, fletcher16(x, 1));
+  assertEqual(0, fletcher32(y, 1));
+  assertEqual(0, fletcher64(z, 1));
+}
+
+
+unittest(test_FF_2)
+{
+  uint8_t x[1] = 0xFF;
+  uint16_t y[1] = 0xFFFF;
+  uint32_t z[1] = 0xFFFFFFFF;
+  
+  Fletcher16 FL16;
+  FL16.begin();
+  assertEqual(0, FL16.getFletcher());
+  FL16.begin(0xFF, 0);
+  assertEqual(0, FL16.getFletcher());
+  FL16.add(0xFF);
+  assertEqual(0, FL16.getFletcher());
+
+  Fletcher32 FL32;
+  FL32.begin();
+  assertEqual(0, FL32.getFletcher());
+  FL32.begin(0xFFFF, 0);
+  assertEqual(0, FL32.getFletcher());
+  FL32.add(0xFFFF);
+  assertEqual(0, FL32.getFletcher());
+
+  Fletcher64 FL64;
+  FL64.begin();
+  assertEqual(0, FL64.getFletcher());
+  FL64.begin(0xFFFFFFFF, 0);
+  assertEqual(0, FL64.getFletcher());
+  FL64.add(0xFFFFFFFF);
+  assertEqual(0, FL64.getFletcher());
+}
+
+
 unittest_main()
 
 // --------
